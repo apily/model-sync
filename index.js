3835993c2e7443d1f4916abb49abccaa9eb3ba19
Enrico Marino <enrico.marino@email.com>
2013-03-11 16:03:59 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+
+/**
+ * model-sync
+ * Model sync model
+ *
+ * @copyright 2012 Enrico Marino and Federico Spini
+ * @license MIT
+ */ 
+
+/**
+ * Dependencies
+ */
+
+var request = require('request');
+
+/**
+ * Expose `sync`
+ */
+
+var sync = module.exports = {};
+
+/**
+ * create
+ * create the model
+ * 
+ * @param {Function} callback callback
+ * @api public
+ */
+
+sync.create = function (callback) {
+  
+};
+
